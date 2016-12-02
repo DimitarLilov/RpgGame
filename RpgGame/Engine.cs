75550a70c8cab550e7ed20f8b0a5cfb56296db40
DimitarLilov <Dimitar.lilov.mici@gmail.com>
2016-12-02 11:15:56 +0200

    Game

---
+++ RpgGame/Engine.cs
@@ -0,0 +1,39 @@
+﻿using RLNET;
+
+namespace RpgGame
+{
+    public class Engine
+    {
+        private static readonly int screenWidth = 100;
+        private static readonly int screenHeight = 70;
+
+        private static RLRootConsole rootConsole;
+
+        public void Run()
+        {
+
+            string fontFileName = "../../Resources/terminal8x8.png";
+
+            string consoleTitle = "Rpg Game";
+
+            rootConsole = new RLRootConsole(fontFileName, screenWidth, screenHeight,
+              8, 8, 1f, consoleTitle);
+
+            rootConsole.Update += OnRootConsoleUpdate;
+
+            rootConsole.Render += OnRootConsoleRender;
+
+            rootConsole.Run();
+        }
+
+        private static void OnRootConsoleUpdate(object sender, UpdateEventArgs e)
+        {
+            rootConsole.Print(10, 10, "It worked!", RLColor.White);
+        }
+
+        private static void OnRootConsoleRender(object sender, UpdateEventArgs e)
+        {
+            rootConsole.Draw();
+        }
+    }
+}
